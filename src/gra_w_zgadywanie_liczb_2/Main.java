a7e2d767603ef7e2525c6e16e19a4042a5c54819
Krzysztof Tomala <krzysiektom@op.pl>
2018-11-06 09:27:37 +0100

    gra_w_zgadywanie_liczb_2

--- src/gra_w_zgadywanie_liczb_2/Main.java
+++ src/gra_w_zgadywanie_liczb_2/Main.java
@@ -4,37 +4,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final String ZA_DUZO="za dużo";
-        final String ZA_MALO="za mało";
-        final String ZGADLES="zgadłeś";
+        final String TOO_MUCH="za dużo";
+        final String TOO_LITTLE="za mało";
+        final String GUESS="zgadłeś";
 
         System.out.println("Pomyśl liczbę od 0 do 1000 a ja ją zgadnę w max. 10 próbach");
         int min=0, max=1000;
-        boolean zgadlem=false;
+        boolean isGuess=false;
         Scanner scanner=new Scanner(System.in);
-        while (!zgadlem) {
+        while (!isGuess) {
             int guess = (max - min) / 2 + min;
             System.out.println("Zgaduję " + guess);
             System.out.println("Wprowadz 'za dużo', 'za mało' lub 'zgadłeś'");
 
-            boolean czyPrawidlowaOdpowiedz=false;
-            String odpowiedz="";
-            while (!czyPrawidlowaOdpowiedz){
-                odpowiedz=scanner.nextLine();
-                if(odpowiedz.equals(ZA_DUZO)||odpowiedz.equals(ZA_MALO)||odpowiedz.equals(ZGADLES)){
-                    czyPrawidlowaOdpowiedz=true;
+            boolean isCorrectEnteredString=false;
+            String enteredString="";
+            while (!isCorrectEnteredString){
+                enteredString=scanner.nextLine();
+                if(enteredString.equals(TOO_MUCH)||enteredString.equals(TOO_LITTLE)||enteredString.equals(GUESS)){
+                    isCorrectEnteredString=true;
                 } else {
                     System.out.println("Wprowadz odpowiednią odowiedz");
                 }
             }
-            switch (odpowiedz) {
-                case ZGADLES:
-                    zgadlem = true;
+            switch (enteredString) {
+                case GUESS:
+                    isGuess = true;
                     break;
-                case ZA_DUZO:
+                case TOO_MUCH:
                     max = guess;
                     break;
-                case ZA_MALO:
+                case TOO_LITTLE:
                     min = guess;
                     break;
             }
